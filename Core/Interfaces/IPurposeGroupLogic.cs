9879b30575fbdd68ad2e4e768e57877d76be8b0c
NeaEnot <oleg.kozloss@yandex.ru>
2022-03-09 09:22:42 +0400

    Интерфейс логики для цели и для группы целей

---
+++ Core/Interfaces/IPurposeGroupLogic.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface IPurposeGroupLogic
+    {
+        void Create(PurposeGroup model);
+        void Create(List<PurposeGroup> models);
+        List<PurposeGroup> Read();
+        void Update(PurposeGroup model);
+        void Delete(string id);
+    }
+}
---
+++ Core/Interfaces/IPurposeLogic.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface IPurposeLogic
+    {
+        void Create(Purpose model);
+        void Create(List<Purpose> models);
+        List<Purpose> Read(string groupId);
+        void Update(Purpose model);
+        void Delete();
+        void Delete(string id);
+        void Delete(List<string> ids);
+    }
+}
