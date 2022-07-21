fd24903e7fc845cf673b9f1647169f86a308a738
NeaEnot <oleg.kozloss@yandex.ru>
2022-07-21 22:15:54 +0400

    Применение цветовой схемы для календаря XF

--- GroundhogMobile/GroundhogMobile/CalendarPage.xaml.cs
+++ GroundhogMobile/GroundhogMobile/CalendarPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GroundhogMobile.Formatters;
+using System;
 using Xalendar.View.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,13 +12,22 @@ namespace GroundhogMobile
         public CalendarPage()
         {
             InitializeComponent();
+
+            CreateCalendar();
         }
 
         private void CalendarView_DaySelected(DaySelected obj)
         {
             Navigation.PushAsync(new TasksPage(obj.DateTime));
 
+            CreateCalendar();
+        }
+
+        private void CreateCalendar()
+        {
             CalendarView calendar = new CalendarView();
+            calendar.Theme = Resources;
+            calendar.DaysOfWeekFormatter = new DayOfWeek2CaractersFormatter();
             calendar.DaySelected += CalendarView_DaySelected;
             calendar.FirstDayOfWeek = DayOfWeek.Monday;
 
---
+++ GroundhogMobile/GroundhogMobile/Formatters/DayOfWeek2CaractersFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Xalendar.Api.Interfaces;
+
+namespace GroundhogMobile.Formatters
+{
+    public class DayOfWeek2CaractersFormatter : IDayOfWeekFormatter
+    {
+        private static Dictionary<DayOfWeek, string> days =
+            new Dictionary<DayOfWeek, string>
+            {
+                { DayOfWeek.Sunday, "Вс" },
+                { DayOfWeek.Monday, "Пн" },
+                { DayOfWeek.Tuesday, "Вт" },
+                { DayOfWeek.Wednesday, "Ср" },
+                { DayOfWeek.Thursday, "Чт" },
+                { DayOfWeek.Friday, "Пт" },
+                { DayOfWeek.Saturday, "Сб" }
+            };
+
+        public string Format(DayOfWeek dayOfWeek)
+        {
+            return days[dayOfWeek];
+        }
+    }
+}
