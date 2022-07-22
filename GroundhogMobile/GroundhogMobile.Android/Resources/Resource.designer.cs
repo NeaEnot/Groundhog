50a87ac7d7ff373899acdd5a430907ad47041ba9
NeaEnot <oleg.kozloss@yandex.ru>
2022-07-22 18:22:54 +0400

    Исправить отображение иконки в Android приложении

--- GroundhogMobile/GroundhogMobile.Android/Resources/Resource.designer.cs
+++ GroundhogMobile/GroundhogMobile.Android/Resources/Resource.designer.cs
@@ -19842,13 +19842,19 @@ namespace GroundhogMobile.Droid
 			public const int calendar = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon = 2131492865;
+			public const int calendar_adaptive_back = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int icon_round = 2131492866;
+			public const int calendar_adaptive_fore = 2131492866;
 			
 			// aapt resource value: 0x7F0C0003
-			public const int launcher_foreground = 2131492867;
+			public const int icon = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int icon_round = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int launcher_foreground = 2131492869;
 			
 			static Mipmap()
 			{
