d6b7257735d68debaea19e5e3d5af52dddb82c5d
NeaEnot <oleg.kozloss@yandex.ru>
2022-07-23 01:12:41 +0400

    Рендер Editor в XF

---
+++ GroundhogMobile/GroundhogMobile.Android/Renderers/EditorRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(GroundhogMobile.Droid.Renderers.EditorRenderer))]
+namespace GroundhogMobile.Droid.Renderers
+{
+    class EditorRenderer : Xamarin.Forms.Platform.Android.EditorRenderer
+    {
+        [Obsolete]
+        public EditorRenderer(Context context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf(ColorConverter.ToAndroidColor((Color)App.Current.Resources["Additional text"]));
+            }
+        }
+    }
+}
