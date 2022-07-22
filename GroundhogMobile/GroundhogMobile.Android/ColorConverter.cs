0057416d9a484cee5e98123cb02b20e2736ea706
NeaEnot <oleg.kozloss@yandex.ru>
2022-07-23 01:08:23 +0400

    Рендер Entry в XF

---
+++ GroundhogMobile/GroundhogMobile.Android/ColorConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace GroundhogMobile.Droid
+{
+    internal static class ColorConverter
+    {
+        public static Android.Graphics.Color ToAndroidColor(Xamarin.Forms.Color from)
+        {
+            Android.Graphics.Color color = 
+                new Android.Graphics.Color((byte)(from.R * byte.MaxValue),
+                                           (byte)(from.G * byte.MaxValue),
+                                           (byte)(from.B * byte.MaxValue));
+
+            return color;
+        }
+    }
+}
---
+++ GroundhogMobile/GroundhogMobile.Android/Renderers/EntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(GroundhogMobile.Droid.Renderers.EntryRenderer))]
+namespace GroundhogMobile.Droid.Renderers
+{
+    public class EntryRenderer : Xamarin.Forms.Platform.Android.EntryRenderer
+    {
+        [Obsolete]
+        public EntryRenderer(Context context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf(ColorConverter.ToAndroidColor((Color)App.Current.Resources["Additional text"]));
+            }
+        }
+    }
+}
