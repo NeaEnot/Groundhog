190ee31289d156159360da7d033162cee9343501
NeaEnot <oleg.kozloss@yandex.ru>
2022-07-23 05:27:02 +0400

    Рендер Picker в XF

---
+++ GroundhogMobile/GroundhogMobile.Android/Renderers/PickerRenderer.cs
@@ -0,0 +1,26 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(GroundhogMobile.Droid.Renderers.PickerRenderer))]
+namespace GroundhogMobile.Droid.Renderers
+{
+    public class PickerRenderer : Xamarin.Forms.Platform.Android.AppCompat.PickerRenderer
+    {
+        [Obsolete]
+        public PickerRenderer(Context context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.BackgroundTintList = ColorStateList.ValueOf(ColorConverter.ToAndroidColor((Color)App.Current.Resources["Additional text"]));
+            }
+        }
+    }
+}
