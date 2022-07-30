f2cdc2a95a55c57c471138554771cff1d3dc69ef
NeaEnot <oleg.kozloss@yandex.ru>
2022-07-30 21:24:43 +0400

    Интерфейс логики работы с заметками

---
+++ Core/Interfaces/INoteLogic.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface INoteLogic
+    {
+        void Create(Note model);
+        void Create(List<Note> models);
+        List<Note> Read();
+        Note Read(string id);
+        void Update(Note model);
+        void Delete(string id);
+    }
+}
