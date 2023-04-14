c31dc6e83064ac89e12403153d8bf932d31e7ef1
NeaEnot <oleg.kozloss@yandex.ru>
2023-04-14 17:23:31 +0400

    Реализация функции скачивания языков

--- YandexDisk/Language/NetworkLanguageLogic.cs
+++ YandexDisk/Language/NetworkLanguageLogic.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using Core;
+using Core.Interfaces.Network;
+using Core.Models.Storage;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using YandexDisk.Client;
+using YandexDisk.Client.Clients;
+using YandexDisk.Client.Http;
+using YandexDisk.Storage;
 
 namespace YandexDisk.Language
 {
-    internal class NetworkLanguageLogic
+    internal class NetworkLanguageLogic : INetworkLogic
     {
+        public Regex ConnectionStringExpr => ConnectionString.connectionStringExpr;
+        public string ConnectionStringFormat => "token=xxxxx;path=path/to/file.ext";
+
+        private IDiskApi diskApi;
+        private string cloudLanguagesFile = $@"{GroundhogContext.StoragePath}\cloudLanguagesFile.json";
+
+        public void Connect(Func<string> getCode)
+        {
+            diskApi = new DiskHttpApi(ConnectionString.Token);
+        }
+
+        public bool IsConnected() => diskApi != null;
+
+        public void Load()
+        {
+            try
+            {
+                FileInfo file = new FileInfo(cloudLanguagesFile);
+                if (file.Exists)
+                    file.Delete();
+
+                System.Threading.Tasks.Task threadTask = diskApi.Files.DownloadFileAsync(ConnectionString.Path, cloudLanguagesFile);
+                threadTask.Wait();
+
+                LanguagesSerializer.Deserialize(new DirectoryInfo(GroundhogContext.LanguagesPath), File.ReadAllText(cloudLanguagesFile));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{GroundhogContext.Language.ErrorsMessages.FailedToDownloadData}: " + ex.Message);
+            }
+        }
+
+        public void Upload()
+        {
+            using (StreamWriter writer = new StreamWriter(cloudLanguagesFile))
+            {
+                string json = LanguagesSerializer.Serialize(new DirectoryInfo(GroundhogContext.LanguagesPath));
+                writer.Write(json);
+            }
+
+            try
+            {
+                diskApi.Files.UploadFileAsync(ConnectionString.Path, true, cloudLanguagesFile, CancellationToken.None).Wait();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{GroundhogContext.Language.ErrorsMessages.FailedToUploadData}: " + ex.Message);
+            }
+        }
     }
 }
