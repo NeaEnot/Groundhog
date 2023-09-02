0f29af0fcd24e697c76613516fbac5593a021c2d
NeaEnot <oleg.kozloss@yandex.ru>
2023-09-02 18:54:00 +0400

    Интерфейс для бэкапов

---
+++ Core/Interfaces/Network/IBackupLogic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Interfaces.Network
+{
+    public interface IBackupLogic
+    {
+        List<string> Backups { get; }
+
+        void MakeBackup(string key);
+        void RestoreBackup(string key);
+    }
+}
