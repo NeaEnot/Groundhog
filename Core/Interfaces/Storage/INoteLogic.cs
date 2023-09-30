1c4e666a07625746b0c19b8da47c483778178075
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 02:46:06 +0400

    Документация для INoteLogic

--- Core/Interfaces/Storage/INoteLogic.cs
+++ Core/Interfaces/Storage/INoteLogic.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 
 namespace Core.Interfaces.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INoteLogic"]/INoteLogic/*'/>
     public interface INoteLogic
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INoteLogic"]/Create/*'/>
         void Create(Note model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INoteLogic"]/CreateList/*'/>
         void Create(List<Note> models);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INoteLogic"]/Read/*'/>
         List<Note> Read();
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INoteLogic"]/Update/*'/>
         void Update(Note model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INoteLogic"]/Delete/*'/>
         void Delete(string id);
     }
 }
