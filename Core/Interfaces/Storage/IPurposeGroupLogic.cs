4cd2a7f306dac60f55e15c6db1e902cfd6ab8577
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 02:52:21 +0400

    Документация для IPurposeGroupLogic

--- Core/Interfaces/Storage/IPurposeGroupLogic.cs
+++ Core/Interfaces/Storage/IPurposeGroupLogic.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 
 namespace Core.Interfaces.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeGroupLogic"]/IPurposeGroupLogic/*'/>
     public interface IPurposeGroupLogic
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeGroupLogic"]/Create/*'/>
         void Create(PurposeGroup model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeGroupLogic"]/CreateList/*'/>
         void Create(List<PurposeGroup> models);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeGroupLogic"]/Read/*'/>
         List<PurposeGroup> Read();
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeGroupLogic"]/Update/*'/>
         void Update(PurposeGroup model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeGroupLogic"]/Delete/*'/>
         void Delete(string id);
     }
 }
