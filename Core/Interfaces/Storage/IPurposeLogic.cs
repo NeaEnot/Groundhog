45f57ccad63b20a77ac1111ac0a9bddbf4c43eb8
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 03:00:14 +0400

    Документация для IPurposeLogic

--- Core/Interfaces/Storage/IPurposeLogic.cs
+++ Core/Interfaces/Storage/IPurposeLogic.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 
 namespace Core.Interfaces.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/IPurposeLogic/*'/>
     public interface IPurposeLogic
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/Create/*'/>
         void Create(Purpose model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/CreateList/*'/>
         void Create(List<Purpose> models);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/Read/*'/>
         List<Purpose> Read(string groupId);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/Update/*'/>
         void Update(Purpose model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/DeleteAll/*'/>
         void Delete();
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/Delete/*'/>
         void Delete(string id);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IPurposeLogic"]/DeleteList/*'/>
         void Delete(List<string> ids);
     }
 }
