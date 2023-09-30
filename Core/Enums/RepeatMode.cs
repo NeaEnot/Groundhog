812bc1f3b7d53648dc76efa83351b5ad157b7337
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 02:16:59 +0400

    Доработка документации для RepeatMode

--- Core/Enums/RepeatMode.cs
+++ Core/Enums/RepeatMode.cs
@@ -3,11 +3,17 @@
     /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/RepeatMode/*'/>
     public enum RepeatMode
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/None/*'/>
         None,
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/Days/*'/>
         Days,
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/DayOfMonth/*'/>
         DayOfMonth,
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/DayOfYear/*'/>
         DayOfYear,
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/DaysOfWeek/*'/>
         DaysOfWeek,
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="RepeatMode"]/Wathes/*'/>
         Wathes
     }
 }
--- Core/Interfaces/Network/IBackupLogic.cs
+++ Core/Interfaces/Network/IBackupLogic.cs
@@ -2,12 +2,17 @@
 
 namespace Core.Interfaces.Network
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IBackupLogic"]/IBackupLogic/*'/>
     public interface IBackupLogic
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IBackupLogic"]/Backups/*'/>
         List<string> Backups { get; }
 
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IBackupLogic"]/MakeBackup/*'/>
         void MakeBackup(string key);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IBackupLogic"]/RestoreBackup/*'/>
         void RestoreBackup(string key);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="IBackupLogic"]/DeleteBackup/*'/>
         void DeleteBackup(string key);
     }
 }
