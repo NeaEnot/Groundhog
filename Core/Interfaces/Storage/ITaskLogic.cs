13af8408cd5a42e268439f3f1db1a5802b962cb9
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 03:18:27 +0400

    Документация для ITaskLogic

--- Core/Interfaces/Storage/ITaskLogic.cs
+++ Core/Interfaces/Storage/ITaskLogic.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 
 namespace Core.Interfaces.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/ITaskLogic/*'/>
     public interface ITaskLogic
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/Create/*'/>
         void Create(Task model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/CreateList/*'/>
         void Create(List<Task> models);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/Read/*'/>
         List<Task> Read();
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/ReadOne/*'/>
         Task Read(string id);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/Update/*'/>
         void Update(Task model);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ITaskLogic"]/Delete/*'/>
         void Delete(string id);
     }
 }
