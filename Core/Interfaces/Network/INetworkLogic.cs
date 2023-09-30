1cfaefc5acf75c3bde0e25140bba0ca28a9fbf4e
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 02:34:34 +0400

    Документация для INetworkLogic

--- Core/Interfaces/Network/INetworkLogic.cs
+++ Core/Interfaces/Network/INetworkLogic.cs
@@ -3,14 +3,21 @@ using System.Text.RegularExpressions;
 
 namespace Core.Interfaces.Network
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/INetworkLogic/*'/>
     public interface INetworkLogic
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/ConnectionStringExpr/*'/>
         Regex ConnectionStringExpr { get; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/ConnectionStringFormat/*'/>
         string ConnectionStringFormat { get; }
 
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/Connect/*'/>
         void Connect(Func<string> getCode);
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/IsConnected/*'/>
         bool IsConnected();
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/Load/*'/>
         void Load();
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="INetworkLogic"]/Upload/*'/>
         void Upload();
     }
 }
