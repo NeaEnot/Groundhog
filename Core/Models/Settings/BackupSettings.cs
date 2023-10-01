711e51621eb98140506c576467a099faf064979e
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 15:56:52 +0400

    Документация для BackupSettings

--- Core/Models/Settings/BackupSettings.cs
+++ Core/Models/Settings/BackupSettings.cs
@@ -1,8 +1,11 @@
 ﻿namespace Core.Models.Settings
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="BackupSettings"]/BackupSettings/*'/>
     public class BackupSettings
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="BackupSettings"]/AutoCloudBackup/*'/>
         public bool AutoCloudBackup { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="BackupSettings"]/AutoLocalBackup/*'/>
         public bool AutoLocalBackup { get; set; }
     }
 }
