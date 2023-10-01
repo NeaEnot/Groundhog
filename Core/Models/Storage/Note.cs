880435db6dff4a9ea850a6d163a3751d9fdb8f7e
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 16:28:27 +0400

    Документация для Note

--- Core/Models/Storage/Note.cs
+++ Core/Models/Storage/Note.cs
@@ -1,9 +1,13 @@
 ﻿namespace Core.Models.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Note"]/Note/*'/>
     public class Note : CommentedElemet
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Note"]/Id/*'/>
         public string Id { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Note"]/Name/*'/>
         public string Name { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Note"]/Text/*'/>
         public string Text { get; set; }
 
         public override int GetHashCode()
