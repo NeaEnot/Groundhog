8bc8586173bc60f4919d2859e95527af54ee3d96
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 16:40:25 +0400

    Документация для PurposeGroup

--- Core/Models/Storage/PurposeGroup.cs
+++ Core/Models/Storage/PurposeGroup.cs
@@ -1,8 +1,11 @@
 ﻿namespace Core.Models.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="PurposeGroup"]/PurposeGroup/*'/>
     public class PurposeGroup : CommentedElemet
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="PurposeGroup"]/Id/*'/>
         public string Id { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="PurposeGroup"]/Name/*'/>
         public string Name { get; set; }
 
         public override int GetHashCode()
