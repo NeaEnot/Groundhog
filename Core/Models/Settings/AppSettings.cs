39c44ff2ba2aac15596dbea4df0acadcda43779e
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 14:52:00 +0400

    Документация для AppSettings

--- Core/Models/Settings/AppSettings.cs
+++ Core/Models/Settings/AppSettings.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 
 namespace Core.Models.Settings
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/AppSettings/*'/>
     public class AppSettings
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/ConnectionStringStorage/*'/>
         public string ConnectionStringStorage { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/ConnectionStringLanguage/*'/>
         public string ConnectionStringLanguage { get; set; }
 
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/PlanningRanges/*'/>
         public Dictionary<RepeatMode, int> PlanningRanges { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/OptimizationRange/*'/>
         public int OptimizationRange { get; set; }
 
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/ColorSchema/*'/>
         public ColorSchema ColorSchema { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/BackupSettings/*'/>
         public BackupSettings BackupSettings { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="AppSettings"]/Language/*'/>
         public string Language { get; set; }
-
 
         public AppSettings()
         {
