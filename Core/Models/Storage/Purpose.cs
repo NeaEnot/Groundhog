de44eac1c60b9520b160e5a1b88476a8029cfbb6
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 16:35:48 +0400

    Документация для Purpose

--- Core/Models/Storage/Purpose.cs
+++ Core/Models/Storage/Purpose.cs
@@ -1,10 +1,15 @@
 ﻿namespace Core.Models.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Purpose"]/Purpose/*'/>
     public class Purpose : CommentedElemet
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Purpose"]/Id/*'/>
         public string Id { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Purpose"]/GroupId/*'/>
         public string GroupId { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Purpose"]/Text/*'/>
         public string Text { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="Purpose"]/Completed/*'/>
         public bool Completed { get; set; }
 
         public override int GetHashCode()
