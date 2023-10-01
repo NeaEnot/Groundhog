9760936305b4e880d0a0c475a749f0eb7faf8427
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 17:03:51 +0400

    Документация для TaskInstance

--- Core/Models/Storage/TaskInstance.cs
+++ Core/Models/Storage/TaskInstance.cs
@@ -2,11 +2,16 @@
 
 namespace Core.Models.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="TaskInstance"]/TaskInstance/*'/>
     public class TaskInstance : CommentedElemet
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="TaskInstance"]/Id/*'/>
         public string Id { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="TaskInstance"]/TaskId/*'/>
         public string TaskId { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="TaskInstance"]/Date/*'/>
         public DateTime Date { get; set; }
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="TaskInstance"]/Completed/*'/>
         public bool Completed { get; set; }
 
         public override int GetHashCode()
