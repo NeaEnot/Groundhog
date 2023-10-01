2fa000ce42525b34c4ef5d2692ab876cc4a9d641
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 13:51:58 +0400

    Документация для BackupsLanguage

--- Core/Models/Settings/Lang/BackupsLanguage.cs
+++ Core/Models/Settings/Lang/BackupsLanguage.cs
@@ -2,6 +2,7 @@
 
 namespace Core.Models.Settings.Lang
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="BackupsLanguage"]/BackupsLanguage/*'/>
     public class BackupsLanguage
     {
         public string Backup { get; set; }
