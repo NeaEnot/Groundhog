96d23432dfcfc782e9d7afe6e791ba31e6900c82
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 16:13:10 +0400

    Документация для CommentedElemet

--- Core/Models/Storage/CommentedElemet.cs
+++ Core/Models/Storage/CommentedElemet.cs
@@ -1,8 +1,10 @@
 ﻿namespace Core.Models.Storage
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="CommentedElemet"]/CommentedElemet/*'/>
     public abstract class CommentedElemet
     {
         private string comment;
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="CommentedElemet"]/Comment/*'/>
         public string Comment
         {
             get => string.IsNullOrEmpty(comment) ? null : comment;
