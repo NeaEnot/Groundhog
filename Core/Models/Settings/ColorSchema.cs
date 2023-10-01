e02ba2e354ee94b6afbd686b4cff757dc7a12e72
NeaEnot <oleg.kozloss@yandex.ru>
2023-10-01 16:06:39 +0400

    Документация для ColorSchema

--- Core/Models/Settings/ColorSchema.cs
+++ Core/Models/Settings/ColorSchema.cs
@@ -2,8 +2,10 @@
 
 namespace Core.Models.Settings
 {
+    /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ColorSchema"]/ColorSchema/*'/>
     public class ColorSchema
     {
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ColorSchema"]/Colors/*'/>
         public Dictionary<string, string> Colors { get; set; }
 
         public ColorSchema()
@@ -11,6 +13,7 @@ namespace Core.Models.Settings
             Colors = new Dictionary<string, string>();
         }
 
+        /// <include file='CoreDoc.xml' path='CoreDoc/members[@name="ColorSchema"]/ColorSchemaAbsent/*'/>
         public List<string> ColorSchemaAbsent(List<string> keys)
         {
             List<string> absent = new List<string>();
